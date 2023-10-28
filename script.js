560698d121870da64c123ea9548f0442034e230b
Muhammad Hany <dev.mhany@gmail.com>
2023-10-28 16:22:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+function openImage(src, description) {
+    const modal = document.getElementById('modal');
+    const modalImage = document.getElementById('modalImage');
+    const modalDescription = document.getElementById('modalDescription');
+    
+    modal.style.display = 'block';
+    modalImage.src = src;
+    modalDescription.textContent = description;
+  }
+  
+  function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+  }
+  
+  function openImage(src, description) {
+    const modal = document.getElementById('modal');
+    const modalImage = document.getElementById('modalImage');
+    const modalDescription = document.getElementById('modalDescription');
+    const sendMessageButton = document.getElementById('sendMessageButton');
+  
+    modal.style.display = 'block';
+    modalImage.src = src;
+    modalDescription.textContent = description;
+  
+    sendMessageButton.onclick = function() {
+      sendMessage(src, description);
+    }
+  }
+  
+  function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+  }
+  
+  function sendMessage(imageSrc, description) {
+    const phoneNumber = '+201159871985';
+    const message = `هاي, انا مهتم بالمنتج دا! \nوصف الإشتراك: ${description}`;
+    const whatsappUrl = 'https://wa.me/' + phoneNumber + '?text=' + encodeURIComponent(message);
+    window.open(whatsappUrl, '_blank');
+  }
+  
